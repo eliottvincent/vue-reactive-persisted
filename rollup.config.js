d549bc7238a43e3561ee1a5750bb7dba77137314
Eliott Vincent <dev@eliottvincent.com>
2023-01-03 10:35:01 +0100

    Add build configuration

---
+++ rollup.config.js
@@ -0,0 +1,80 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+import pkg from "./package.json";
+
+const banner = `/*!
+ * vue-reactive-persisted v${pkg.version}
+ * (c) ${new Date().getFullYear()} Eliott Vincent
+ * @license MIT
+ */`;
+
+const configs = [
+  {
+    input: "src/index.js",
+    file: "dist/persist.esm-browser.js",
+    format: "es"
+  },
+  {
+    input: "src/index.js",
+    file: "dist/persist.esm-bundler.js",
+    format: "es"
+  },
+  {
+    input: "src/index.cjs.js",
+    file: "dist/persist.global.js",
+    format: "iife",
+    minify: true
+  },
+  {
+    input: "src/index.cjs.js",
+    file: "dist/persist.cjs.js",
+    format: "cjs",
+    env: "development"
+  }
+];
+
+function createEntries() {
+  return configs.map(config => {
+    return createEntry(config);
+  });
+}
+
+function createEntry(config) {
+  const isGlobalBuild = config.format === "iife";
+
+  const c = {
+    external: ["vue"],
+    input: config.input,
+    plugins: [
+      resolve(),
+      commonjs()
+    ],
+    output: {
+      banner,
+      file: config.file,
+      format: config.format,
+      exports: "auto",
+      globals: {
+        vue: "Vue"
+      }
+    },
+    onwarn: (msg, warn) => {
+      if (!/Circular/.test(msg)) {
+        warn(msg);
+      }
+    }
+  };
+
+  if (isGlobalBuild) {
+    c.output.name = "Persist";
+  }
+
+  if (config.minify) {
+    c.plugins.push(terser());
+  }
+
+  return c;
+}
+
+export default createEntries();
