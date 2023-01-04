b270e826a919624af51249d83d1680fb45ef3b5b
Eliott Vincent <dev@eliottvincent.com>
2023-01-04 12:55:30 +0100

    Add more tests

--- test/helpers.js
+++ test/helpers.js
@@ -1,6 +1,10 @@
 import Storage from "dom-storage";
 
-export function initLocalStorage () {
+export function initLocalStorage (key = null, object = null) {
   window.localStorage = new Storage();
   window.localStorage.clear();
+
+  if (key && object) {
+    window.localStorage.setItem(key, object);
+  }
 }
--- test/persist.spec.js
+++ test/persist.spec.js
@@ -1,7 +1,11 @@
 import { initLocalStorage } from "./helpers.js";
-import { reactive } from "vue";
+import { reactive, nextTick } from "vue";
 
 import persist from "./../src/";
+
+const DEFAULT_KEY = "reactive_persisted";
+const CUSTOM_KEY = "custom_key";
+
 
 it("should throw an error when no reactive object is passed", () => {
   initLocalStorage();
@@ -23,11 +27,11 @@ it("should return the reactive object", () => {
   expect(JSON.stringify(store)).toBe(JSON.stringify(object));
 });
 
-it("should assert the local store (default key)", () => {
+it("should assert the local store", () => {
   initLocalStorage();
   persist(reactive({ foo: "bar" }));
 
-  expect(window.localStorage.getItem("reactive_persisted")).toBe(JSON.stringify({}));
+  expect(window.localStorage.getItem(DEFAULT_KEY)).toBe(JSON.stringify({}));
 });
 
 it("should assert the local store (custom key)", () => {
@@ -37,10 +41,79 @@ it("should assert the local store (custom key)", () => {
     reactive({ foo: "bar" }),
 
     {
-      key : "custom_key"
+      key : CUSTOM_KEY
     }
   );
 
-  expect(window.localStorage.getItem("reactive_persisted")).not.toBe(JSON.stringify({}));
-  expect(window.localStorage.getItem("custom_key")).toBe(JSON.stringify({}));
+  expect(window.localStorage.getItem(DEFAULT_KEY)).not.toBe(JSON.stringify({}));
+  expect(window.localStorage.getItem(CUSTOM_KEY)).toBe(JSON.stringify({}));
+});
+
+it("should sync the reactive object with local store on initialization", () => {
+  initLocalStorage(DEFAULT_KEY, JSON.stringify({ foo: "baz" }));
+  const state = reactive({ foo: "bar" });
+
+  persist(state);
+
+  expect(JSON.stringify(state)).toBe(JSON.stringify({foo: "baz"}));
+});
+
+it("should sync the reactive object with local store on initialization (merge)", () => {
+  initLocalStorage(DEFAULT_KEY, JSON.stringify({ bar: "foo" }));
+  const state = reactive({ foo: "bar" });
+
+  persist(state);
+
+  expect(JSON.stringify(state)).toBe(JSON.stringify({ foo: "bar", bar: "foo" }));
+});
+
+it("should not sync the reactive object with local store on initialization (invalid JSON)", () => {
+  initLocalStorage(DEFAULT_KEY, "<invalid JSON>");
+  const state = reactive({ foo: "bar" });
+
+  persist(state);
+
+  expect(JSON.stringify(state)).toBe(JSON.stringify({foo: "bar"}));
+});
+
+it("should not sync the reactive object with local store on initialization (null)", () => {
+  initLocalStorage(DEFAULT_KEY, "<invalid JSON>");
+  const state = reactive({ foo: "bar" });
+
+  persist(state);
+
+  expect(JSON.stringify(state)).toBe(JSON.stringify({foo: "bar"}));
+});
+
+it("should persist the changed reactive object back to local store", async () => {
+  initLocalStorage();
+  const state = reactive({ foo: "bar" });
+
+  persist(state);
+
+  state.foo = "baz";
+
+  await nextTick();
+
+  expect(window.localStorage.getItem(DEFAULT_KEY)).toBe(JSON.stringify({foo: "baz"}));
+});
+
+it("should persist the changed reactive object back to local store (custom key)", async () => {
+  initLocalStorage();
+  const state = reactive({ foo: "bar" });
+
+  persist(
+    state,
+
+    {
+      key : CUSTOM_KEY
+    }
+  );
+
+  state.foo = "baz";
+
+  await nextTick();
+
+  expect(window.localStorage.getItem(CUSTOM_KEY)).toBe(JSON.stringify({foo: "baz"}));
+  expect(window.localStorage.getItem(DEFAULT_KEY)).not.toBe(JSON.stringify({foo: "baz"}));
 });
